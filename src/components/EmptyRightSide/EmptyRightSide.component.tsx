10efb3e269e1aa0180e577cf69529dbb0dce5e47
afaysalk <afaysalkhaldi@gmail.com>
2023-09-21 10:49:35 -0400

    Empty right side component

---
+++ src/components/EmptyRightSide/EmptyRightSide.component.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Container from '@mui/material/Container';
+
+import Typography from '@mui/material/Typography';
+
+const EmptyRightSide:React.FC = () => {
+	
+  return (
+		<Typography component='h1' variant="h2" > Select a Pokemon </Typography>
+  )
+};
+
+export default EmptyRightSide;
